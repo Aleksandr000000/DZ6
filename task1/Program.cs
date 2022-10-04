64c6822a41380bb6da6b6d99ca1a7a054c05842a
Aleksandr <MrRotod@gmail.com>
2022-10-04 15:50:12 +0500

    задачи на проверку

---
+++ task1/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+void FillArray(int [] arr) // метод ручного заполнения массива числами
+{
+    
+    for (int i = 0; i < arr.Length; i ++ )
+    {
+        Console.Write("Введите  число: ");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    } 
+}
+
+void PrintArray(int [] arr) // метод вывода этого массива
+{
+
+    Console.WriteLine("Создали массив: ");
+    for (int i= 0; i < arr.Length; i++ )
+    {
+        Console.Write(arr[i]+ " ");
+    }
+     
+}
+
+void SummPozitive(int [] arr) // Метод подсчета 
+{   
+    int sum = 0;
+    for (int i= 0; i < arr.Length; i++ )
+    {
+        if (arr[i] > 0)
+        sum++;
+        
+    }     
+    Console.WriteLine($"=>  {sum} элемента(ов) больше 0");
+}
+
+
+Console.WriteLine("Количество чисел в массиве? ");
+int[] array = new int [Convert.ToInt32(Console.ReadLine())]; 
+FillArray(array);
+PrintArray(array);
+SummPozitive(array);
